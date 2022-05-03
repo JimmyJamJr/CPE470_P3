160ec8e72d85025bbb7c141a5d61550dd3dc09e6
Jimson Huang <jimsonjy@gmail.com>
2022-05-03 16:18:06 -0700

    Finished translating find_neighbors code into Python

--- find_neighbors.py
+++ find_neighbors.py
@@ -1,17 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from numpy.random import default_rng
 import numpy.linalg
-import numpy.matlib
 
 
-def find_neighbors(nodes,r,n, delta_t_update):
+def find_neighbors(nodes, r, n, delta_t_update):
     num_nodes = nodes.shape[0]
     dif = np.zeros(shape=(num_nodes, num_nodes, n))
-    # print(dif.shape)
-    # print(dif)
     distance_alpha = np.zeros(shape=(num_nodes,num_nodes))
-    Nei_agent = np.zeros(shape=(num_nodes, num_nodes, 1))
+    Nei_agent = np.zeros(shape=(num_nodes, num_nodes - 1, 1))
 
     for i in range(num_nodes):
         dif[i] = np.tile(np.array(nodes[i,:]), (num_nodes, 1)) - nodes
@@ -19,15 +14,27 @@ def find_neighbors(nodes,r,n, delta_t_update):
         d_tmp = np.zeros(shape=(num_nodes, 1))
         for j in range(num_nodes):
             d_tmp[j,:] = np.linalg.norm(tmp[j,:])
-        # print(d_tmp.shape)
-        # print(distance_alpha.shape)
-        # print(distance_alpha[i,:].shape)
-        # print(d_tmp)
         distance_alpha[i,:] = d_tmp.ravel()
 
     for k in range(num_nodes):
-        # print(distance_alpha[:,k])
-        # print(np.logical_and(distance_alpha[:,k] < r, distance_alpha[:,k] != 0))
-        Nei_agent[k] = np.logical_and(distance_alpha[:,k] < r, distance_alpha[:,k] != 0).reshape(10, 1)
+        out = np.full((9,1), np.nan)
+        indices = np.nonzero(np.logical_and(distance_alpha[:,k] < r, distance_alpha[:,k] != 0.))[0]
+        out.ravel()[:len(indices)] = indices
+        Nei_agent[k] = out
 
+
+    A = np.zeros(shape=(10, 10))
+    for i in range(num_nodes):
+        Nei_nodes = nodes[Nei_agent[i][~np.isnan(Nei_agent[i])].ravel().astype(int), :]
+        for j in range(num_nodes):
+            dist_2nodes = np.linalg.norm(nodes[j,:] - nodes[i,:])
+
+            if dist_2nodes == 0:
+                A[i,j] = 0
+            elif dist_2nodes < r and dist_2nodes != 0:
+                A[i,j] = 1
+            elif dist_2nodes > r:
+                A[i,j] = 0
+
+    return Nei_agent, A
     
--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import numpy.linalg
 
 from find_neighbors import find_neighbors
 
-r = 1 # Communication rage
+r = 1. # Communication rage
 num_nodes = 10 # Number of nodes
 n=2 # Number of dimensions
 delta_t_update = 0.008
 
 rng = default_rng()
-nodes = rng.uniform(size=(num_nodes, n))
+nodes = rng.uniform(low=0.01, high=0.99, size=(num_nodes, n))
 # Add measurement for each node: yi= theta + v_i, or : mi= theta + v_i
 nodes_va = 50. * np.ones(shape=(num_nodes, 1)) + 1 * rng.normal(size=(num_nodes, n))
 nodes_va0 = nodes_va
